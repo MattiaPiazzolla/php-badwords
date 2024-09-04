1a0ae93b94af1b1f443cef0ff0d3f5b7affe0243
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-04 16:04:29 +0200

    [UPD] added an srt replace that will replace the badword with ***

--- request.php
+++ request.php
@@ -2,4 +2,17 @@
 <?php
     $paragraph = $_GET["paragraph"];
     $badword = $_GET["badword"];
+    // Reolace the badword with *** inside paraghtaph 
+    $new_word = str_replace($badword, "***", $paragraph);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP request</title>
+</head>
+<body>
+    
+</body>
+</html>
